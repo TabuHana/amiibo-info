bd90bde22d86f1f95eb2d7144a795a2083681d89
Tabu Hana <taboodog@hotmail.com>
2022-06-08 19:30:04 -0700

    showcase component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { Button, Form, Container, Row, Col, Carousel } from "react-bootstrap";
+import { Container, Row, Col, } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Title from './components/Title';
 import Search from './components/Search';
+import Showcase from './components/Showcase'
 
 // https://amiiboapi.com/api/amiibo/?character=(character name)
 
@@ -15,45 +16,7 @@ function App() {
           <Title />
 
           {/* Carousel Section */}
-          <Row>
-            <Carousel>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="holder.js/800x400?text=First slide&bg=373940"
-                  alt="First slide"
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="holder.js/800x400?text=Second slide&bg=282c34"
-                  alt="Second slide"
-                />
-
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="holder.js/800x400?text=Third slide&bg=20232a"
-                  alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </Row>
+          <Showcase />
 
           {/* Search Form */}
           <Search />
