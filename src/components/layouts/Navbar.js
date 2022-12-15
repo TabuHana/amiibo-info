1ffbec72f7b35a92784587b406ee346c00e331dc
Tabu Hana <taboodog@hotmail.com>
2022-12-15 14:50:17 -0800

    nav & footer

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2 btn-ghost rounded-btn">
+          <Link to='/' className='text-lg font-bold'>
+            Amiibo Info
+          </Link>
+        </div>
+
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn' >
+              About
+            </Link>
+          </div>
+        </div>
+
+      </div>
+    </nav>
   );
 };
 export default Navbar;
