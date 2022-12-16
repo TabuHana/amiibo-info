002cef1987548338fd548253148c9a8200104102
Tabu Hana <taboodog@hotmail.com>
2022-12-15 19:13:05 -0800

    few style changes, added icons, footer about

--- src/App.js
+++ src/App.js
@@ -6,32 +6,9 @@ import About from "./pages/About";
 import Amiibo from "./pages/Amiibo";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-// https://amiiboapi.com/api/amiibo/?character=(character name)
+
 
 function App() {
-
-  //search
-  // const searchAmiibo = async (search) => {
-  //   let characterName = search.search.toUpperCase()
-  //     let amiiboAPI = await fetch(`https://amiiboapi.com/api/amiibo/?character=${characterName}`)
-
-  //     if(amiiboAPI.status === 200) {
-  //       let data = await amiiboAPI.json()
-  //       let amiiboData = data.amiibo
-  //       setAmiibo(amiiboData)
-  //       setTitle(characterName)
-
-  //     } else if (amiiboAPI.status === 404) {
-  //       console.log('invald')
-  //       setTitle('Error')
-  //     } else {
-  //       console.log("Something didn't go right... I just don't know what")
-  //       setTitle('Wha...')
-  //     }
-
-
-
-  // }
 
   return (
     <AmiiboProvider>
@@ -41,7 +18,6 @@ function App() {
           <Navbar />
 
           <main className="container mx-auto px-3 pb-12">
-            {/* @todo - add home, about, amiibo, 404  + routes */ }
             <Routes>
               <Route path="/" element={ <Home /> } />
               <Route path="/about" element={ <About /> } />
@@ -59,5 +35,3 @@ function App() {
 }
 
 export default App;
-
-// @todo - switch to sass, better for resume :(
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+import { FaHome } from 'react-icons/fa';
+
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
-export default About
+    <div className="text-info-content">
+      <h1 className="text-6xl mb-4">Amiibo Info</h1>
+      <p>Amiibo Info is a <strong><a href="https://bsandfer.github.io/amiibo-locator/" target='_blank' rel='noreferrer'>revamp</a></strong> of a project I did in school, with the goal of <u>making it easy to
+        find information about your character's Amiibos</u>.
+      </p>
+      <p>This app allows you to search by <u>game series or character name</u> to view all possible Amiibo's.</p>
+      <p>Powered by <strong><a href="https://www.amiiboapi.com/" target='_blank' rel='noreferrer'>AmiiboAPI</a></strong>!</p>
+      <p><small>PS~ The bold bits are clickable! 😉</small></p>
+      <Link to='/' className='btn btn-secondary btn-sm my-5'>
+        <FaHome className='mr-2' />
+        Return Home
+      </Link>
+    </div>
+  );
+};
+export default About;
